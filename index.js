c44cf685d13cffe78c04d33248407a1e26f7a634
Nadezhda Alkhazova <shailer5407@gmail.com>
2023-10-22 17:35:57 +0300

    Add info from file first.txt to file second.txt

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const fs = require("fs");
 
 fs.writeFileSync("second.txt", "");
 fs.writeFileSync("second.txt", "I like Node JS");
+
+const dateToAdd = fs.readFileSync("first.txt", "utf-8");
+fs.appendFileSync("second.txt", "\n" + dateToAdd);
